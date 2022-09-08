5723a8c0ee6fc9781c62bdb8166379eb10efd5e2
Zayn! <86161489+ItsZaynMalik@users.noreply.github.com>
2022-09-08 11:15:03 +0530

    Update connect.js

--- Database/connect.js
+++ Database/connect.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    if(!process.env.mongo){
+    if(!client.config.mongo){
         console.warn(`[DATABASE] For database access, MongoDB URI, is required in .env file.`)
         return
     }
-    mongoose.connect(process.env.mongo, {
+    mongoose.connect(client.config.mongo, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
